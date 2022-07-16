f7e83658b9daa489fb6a52bff9eb73888612129d
sameh <samehbenali69@gmail.com>
2022-07-16 18:44:08 +0100

    initiale api v1.0.1

---
+++ models/Post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const postSchema = new mongoose.Schema({
+    title :{
+        type: String,
+        required:true,
+        unique: true
+    },
+    desc :{
+        type: String
+    },
+    photo :{
+        type: String
+    },
+    username :{
+        type: String,
+        required: true
+    },
+    categories :{
+        type: Array
+    }
+},{
+    timestamps: true
+})
+
+module.exports = mongoose.model("Post",postSchema)
---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const router = require("express").Router()
+const User = require("../models/User")
+const bcrypt = require("bcrypt")
+
+router.put("/:id",async (req,res)=>{
+    if (req.body.userId===req.params.id){
+        if (req.body.password){
+            const salt = await bcrypt.genSalt(13)
+            req.body.password = await bcrypt.hash(req.body.password,salt)
+        }
+        try{
+            const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+                $set: req.body
+            })
+            res.status(200).json(updatedUser)
+        }catch(err){
+            res.status(500).json(err)
+        }
+    }else{
+        res.status(401).json("you can update only your account")
+    }
+})
+
+module.exports = router
